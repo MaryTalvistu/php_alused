f95e356cb940ac68f99be19c7c3e281873be128b
Mary Talvistu <marytalvistu@gmail.com>
2020-11-24 23:20:38 +0200

    Yl1 lahendus

---
+++ yl1.php
@@ -0,0 +1,12 @@
+<?php
+// Ülesanne 1. Mary-Ann Talvistu. 24/11/2020
+$nimi = "Kalle";
+$synniaasta = 1988;
+$tahtkuju = "neitsi";
+
+echo "Minu nimi on $nimi.<br> Olen sündinud $synniaasta. aastal.<br> Olen $tahtkuju tähtkujust.<br>";
+
+
+echo '“It’s My Life” – Dr. Alban<br>';
+
+echo '(\(\<br>( -.-)<br>o_(")(")';
