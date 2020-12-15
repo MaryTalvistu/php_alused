8b00474a01d69c81c07ce1f84bbddfcad0e259de
Mary Talvistu <marytalvistu@gmail.com>
2020-12-15 17:32:49 +0200

    Yl6 viimased

--- yl6.php
+++ yl6.php
@@ -1,6 +1,11 @@
 <?php
 //Ülesanne 6. Mary-Ann Talvistu. 10/12/2020
 
+
+//loo arvud 1-100
+//loo pärast iga 10 arvu reavahetus
+//lisa iga arvu järele punkt (N: 1. 2. 3. jne…)
+
 for ($nr = 1; $nr <= 100; $nr++) {
     if($nr%10-1==0){
         echo '<br>';
@@ -10,18 +15,24 @@ for ($nr = 1; $nr <= 100; $nr++) {
 
 echo '<br><br>';
 
+
+//koosta tärnidest horisontaalne rida
 for($rida=1; $rida<=10; $rida++){
         echo '*';
     }
 
 echo '<br><br>';
 
+
+//koosta tärnidest vertikaalne rida
 for($rida=1; $rida<=10; $rida++){
     echo '*<br>';
 }
 
 echo '<br><br>';
 
+
+//loo tsükli abil tärnidest ruut, mille suuruse lisab kasutaja läbi veebivormi
 $t2rne = $_GET['a'];
 
 for($rida=1; $rida<=$t2rne; $rida++){
@@ -33,6 +44,8 @@ for($rida=1; $rida<=$t2rne; $rida++){
 
 echo '<br><br>';
 
+
+//väljasta paarisarvud 10-1
 for ($nr = 10; $nr >= 1; $nr--) {
     if($nr%2==0){
         echo $nr.'<br>';
@@ -41,12 +54,36 @@ for ($nr = 10; $nr >= 1; $nr--) {
 
 echo '<br><br>';
 
+
+//tekita arvud 1-100, aga kuva kolmega jagunevad arvud (N: 3, 6, 9 jne.)
 for ($nr = 1; $nr <= 100; $nr++) {
     if($nr%3==0){
         echo $nr . ' ';
     }
 }
 
+echo '<br><br>';
+
+
+//tekita tüdrukute ja poiste massiivid (võrdsed)
+//väljasta poiste ja tüdrukute paarid erinevatel ridadel
 $tydrukud=array('Mari', 'Karin', 'Mall', 'Tiina');
 $poisid=array('Karl', 'Mart', 'Ain', 'Villu');
 
+for($nimi=0;$nimi<count($tydrukud);$nimi++){
+    echo $tydrukud[$nimi].' ja '. $poisid[$nimi].'<br>';
+}
+
+echo '<br><br>';
+
+
+//tee poiste ja tüdrukute massiividest koopiad
+//tekita suvalistest tüdrukutest ja poistest koopia (nimed ei tohi korduda)
+$tydrukud=array('Mari', 'Karin', 'Mall', 'Tiina');
+$poisid=array('Karl', 'Mart', 'Ain', 'Villu');
+shuffle($tydrukud);
+shuffle($poisid);
+
+for($nimi=0;$nimi<count($tydrukud);$nimi++){
+    echo $tydrukud[$nimi].' ja '. $poisid[$nimi].'<br>';
+}
